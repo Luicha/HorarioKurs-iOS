79c1ca19c12d851345943e8398281b69d62dde77
Luicha <iuriygomez@gmail.com>
2024-03-23 18:23:05 -0300

    Initial Commit

---
+++ HorarioKurs/HorarioKursApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorarioKursApp.swift
+//  HorarioKurs
+//
+//  Created by Jorge Luis Gomez on 23/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct HorarioKursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
